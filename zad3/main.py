244d2ef1aad18b434036bc22716c08f59b56d9ab
Kejniasty <mrlesio777pl@gmail.com>
2025-11-12 09:52:03 +0100

    implemented quaternion division

--- zad3/main.py
+++ zad3/main.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':
     print(q1 + q2)
     print(q1 - q2)
     print(q1 * q2)
+    print(q1 / q2)
 
--- zad3/quaternion.py
+++ zad3/quaternion.py
@@ -1,3 +1,4 @@
+import vector
 from vector import Vector
 
 
@@ -17,6 +18,14 @@ class Quaternion:
     def __mul__(self, other):
         return Quaternion(self.a * other.a - self.vector.dot_product(other.vector),
                           other.vector * self.a + self.vector * other.a + self.vector.cross_product(other.vector))
+
+    def __truediv__(self, other):
+        divider = other.a * other.a + other.vector.dot_product(other.vector)
+        if divider == 0:
+            raise ZeroDivisionError
+        result_scalar = (self.a * other.a + self.vector.dot_product(other.vector)) / divider
+        result_vector = (other.vector * self.a * -1.0 + self.vector * other.a - self.vector.cross_product(other.vector)) * (1.0 / divider)
+        return Quaternion(result_scalar, result_vector)
 
     def __str__(self):
         return f"({str(self.a)}, {str(self.vector)})"

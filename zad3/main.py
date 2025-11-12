50b755d0d8da8880746efbc8a692c20673554b9c
Kejniasty <mrlesio777pl@gmail.com>
2025-11-12 09:38:55 +0100

    implemented most of the quaternion operations

--- zad3/main.py
+++ zad3/main.py
@@ -1,4 +1,10 @@
 from quaternion import Quaternion
+from vector import Vector
 
 if __name__ == '__main__':
-    print("Hello world!")
+    q1 = Quaternion(3, Vector(1, 2, 3))
+    q2 = Quaternion(4, Vector(5, 6, 7))
+    print(q1 + q2)
+    print(q1 - q2)
+    print(q1 * q2)
+
--- zad3/quaternion.py
+++ zad3/quaternion.py
@@ -1,7 +1,22 @@
-class Quaternion:
-    def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+from vector import Vector
 
+
+class Quaternion:
+    def __init__(self, a, vector):
+        self.a = a
+        self.vector = Vector(vector.x, vector.y, vector.z)
+
+    def __add__(self, other):
+        return Quaternion(self.a + other.a,
+                          self.vector + other.vector)
+
+    def __sub__(self, other):
+        return Quaternion(self.a - other.a,
+                          self.vector - other.vector)
+
+    def __mul__(self, other):
+        return Quaternion(self.a * other.a - self.vector.dot_product(other.vector),
+                          other.vector * self.a + self.vector * other.a + self.vector.cross_product(other.vector))
+
+    def __str__(self):
+        return f"({str(self.a)}, {str(self.vector)})"

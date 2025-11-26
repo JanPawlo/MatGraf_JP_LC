a0fc6d46311cd2fc0a3f7a4262cec2e2d02677b6
Jan Pawlowski <jan_pawlowski@outlook.com>
2025-11-26 11:29:10 +0100

    intersection plane_line

--- zad4/plane.py
+++ zad4/plane.py
@@ -10,17 +10,17 @@ class Plane:
     # zapis wewnetrzny: normalna i punkt plaszczyzny
     def __init__(self, a, b, c, d):
         self.normal = Vector(a, b, c)
-        if (a != 0):
-            self.point = Vector(d/a, 0, 0)
-        elif (b != 0):
-            self.point = Vector(0, d/b, 0)
-        elif (c != 0):
-            self.point = Vector(0, 0, d/c)
-        else:
-            raise ValueError("Plaszczyzna 0,0,0 to zadna plaszczyzna!")
+        self.d = d
+        # if (a != 0):
+        #     self.point = Vector(d/a, 0, 0)
+        # elif (b != 0):
+        #     self.point = Vector(0, d/b, 0)
+        # elif (c != 0):
+        #     self.point = Vector(0, 0, d/c)
+        # else:
+        #     raise ValueError("Plaszczyzna 0,0,0 to zadna plaszczyzna!")
+
         
-        
-    
     
 
         

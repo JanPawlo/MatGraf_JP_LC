f1365b55d1f1858a093d3cd18ea708eb459a473a
Kejniasty <mrlesio777pl@gmail.com>
2025-12-09 19:39:34 +0100

    sphere and line intersection

--- zad4/main.py
+++ zad4/main.py
@@ -1,3 +1,5 @@
+from math import sqrt
+from sphere import Sphere
 from vector import Vector
 from line import Line, Segment
 
@@ -10,3 +12,14 @@ if __name__ == '__main__':
     segment1 = Segment(Vector(5, 5, 4), Vector(10, 10, 6))
     segment2 = Segment(Vector(5, 5, 5), Vector(10, 10, 3))
     print(segment1.segment_and_segment_cross(segment2))
+
+    sphere1 = Sphere(sqrt(26), Vector(0, 0, 0))
+    line3 = Line(Vector(3, 1, 2), Vector(5, 3, -4) - Vector(3, -1, -2))
+    point1, point2 = sphere1.sphere_and_line_cross(line3)
+    print(str(point1) + " " + str(point2))
+
+    line4 = Line(Vector(0, 0, -5), Vector(0, 0, 1))
+    sphere2 = Sphere(2, Vector(0, 0, 0))
+
+    point1, point2 = sphere2.sphere_and_line_cross(line4)
+    print(str(point1) + " " + str(point2))

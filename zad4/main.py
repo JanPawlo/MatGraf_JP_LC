44a32f42e69e21247180eff6556719d9a83a594e
Kejniasty <mrlesio777pl@gmail.com>
2025-12-10 10:45:00 +0100

    final main edit

--- zad4/main.py
+++ zad4/main.py
@@ -9,24 +9,13 @@ if __name__ == '__main__':
     # zle przepisalem linie wczesniej x d
     line1 = Line(Vector(-2,4, 0), Vector(3,1, 5))
     line2 = Line(Vector(-2,4, 0), Vector(1,-5, 3))
-    print(line1.line_and_line_cross(line2))
+    print("Zadanie 1:")
+    print("pkt. przeciecia prostej z prosta:",
+          line1.line_and_line_cross(line2))
 
-    segment1 = Segment(Vector(5, 5, 4), Vector(10, 10, 6))
-    segment2 = Segment(Vector(5, 5, 5), Vector(10, 10, 3))
-    print(segment1.segment_and_segment_cross(segment2))
-
-    sphere1 = Sphere(sqrt(26), Vector(0, 0, 0))
-    line3 = Line(Vector(3, 1, 2), Vector(5, 3, -4) - Vector(3, -1, -2))
-    point1, point2 = sphere1.sphere_and_line_cross(line3)
-    print(str(point1) + " " + str(point2))
-
-    line4 = Line(Vector(0, 0, -5), Vector(0, 0, 1))
-    sphere2 = Sphere(2, Vector(0, 0, 0))
-
-    point1, point2 = sphere2.sphere_and_line_cross(line4)
-    print(str(point1) + " " + str(point2))
-    
-    
+    print("Zadanie 2:")
+    print("kat. miedzy prosta z prosta:",
+          line1.angle(line2))
     
     # zadanie 3:
     linia3 = Line(Vector(-2, 2, -1), Vector(3, -1, 2))
@@ -52,4 +41,16 @@ if __name__ == '__main__':
     print("Zadanie 6:")
     print("kat przeciecia plaszczyzn:", 
           angle_plane_plane(pl5a, pl5b))
-        
+
+    segment1 = Segment(Vector(5, 5, 4), Vector(10, 10, 6))
+    segment2 = Segment(Vector(5, 5, 5), Vector(10, 10, 3))
+
+    print("Zadanie 7:")
+    print("pkt. przeciecia odcinkow:",
+          segment1.segment_and_segment_cross(segment2))
+
+    sphere1 = Sphere(sqrt(26), Vector(0, 0, 0))
+    line3 = Line(Vector(3, 1, 2), Vector(5, 3, -4) - Vector(3, -1, -2))
+    point1, point2 = sphere1.sphere_and_line_cross(line3)
+    print("Zadanie 8:")
+    print("pkt. przeciecia sfery i prostej: " + str(point1) + " " + str(point2))

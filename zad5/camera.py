eab5cc2cdba0922563077ac22d8c367d71aec55a
Kejniasty <mrlesio777pl@gmail.com>
2025-12-10 11:55:54 +0100

    rays generated as Line class

--- zad5/camera.py
+++ zad5/camera.py
@@ -1,7 +1,6 @@
 from vector import Vector
 from line import Line
 
-
 class Camera(object):
     def __init__(self, width, height, starting_point: "Vector", distance: "Vector"):
         self.width = width
@@ -15,4 +14,11 @@ class Camera(object):
         if y_scalar > 1 or y_scalar < -1:
             raise ValueError("y_scalar must be between -1 and 1")
         ray = Line(self.point, self.distance + Vector((self.width / 2) * x_scalar, (self.height / 2) * y_scalar, 0))
+        return ray
 
+    def generate_ray_list(self, x_pixels, y_pixels):
+        ray_list = []
+        for x in range(x_pixels):
+            for y in range(y_pixels):
+                ray_list.append(self.generate_ray(1 - x / (x_pixels / 2) , 1 - y / (y_pixels / 2 )))
+        return ray_list
--- zad5/main.py
+++ zad5/main.py
@@ -1,5 +1,9 @@
-
+from camera import Camera
+from vector import Vector
 
 if __name__ == '__main__':
     print('hello world')
+    camera1 = Camera(100, 100, Vector(0, 0, -50), Vector(0, 0, 45))
+    for i in camera1.generate_ray_list(60, 60):
+        print(i)
 
